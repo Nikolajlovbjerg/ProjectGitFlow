89f0fb0f0b8c471614cd51835517fe4558287e87
Nikolajlovbjerg <lovbjergr12@gmail.com>
2026-08-31 13:08:39 +0200

    feat: implement login with password verification

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using ProjectGitFlow.Models;
+using ProjectGitFlow.Repositories;
+using ProjectGitFlow.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
+
+public partial class Program { }
